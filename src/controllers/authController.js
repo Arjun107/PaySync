ec06545c81908cc315025eb8abb1dc0361deeeee
Arsh Dawra <arshdawra7@gmail.com>
2025-11-24 03:20:58 +0530

    Jwt token is working in this

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,27 +1,33 @@
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const loginUser = async (req, res) => {
-    try {
-        const { email, password } = req.body
-        const exist = await User.findOne({ email });
-        if (exist) {
-            const match = await bcrypt.compare(password, exist.password)
-            if (match) {
-                return res.status(200).json({ message: "login succeful" });
-            }
-            else {
-                res.status(401).json({ message: "Incorrect password" });
-            }
-        }
+  try {
+    const { email, password } = req.body;
+    const exist = await User.findOne({ email });
+    const secretkey = "my_key";
+    if (exist) {
+      const payload = {
+        userID: exist.id,
+        UserRole: exist.role,
+      };
 
-        else{
-            res.status(404).json({message:"Email Not Exist's"})
-        }
-
-    } catch (error) {
-        res.status(400).json({ message: "Server error", error: error.message });
+      const match = await bcrypt.compare(password, exist.password);
+      if (match) {
+        const token = jwt.sign(payload, secretkey, { expiresIn: "1h" });
+        return res
+          .status(200)
+          .json({ message: "login succeful", token: token });
+      } else {
+        res.status(401).json({ message: "Incorrect password" });
+      }
+    } else {
+      res.status(404).json({ message: "Email Not Exist's" });
     }
-}
+  } catch (error) {
+    res.status(400).json({ message: "Server error", error: error.message });
+  }
+};
 
-module.exports = {loginUser}
+module.exports = { loginUser };
