aaaafa4727de4ec5452ede28d8aa315c3e3fdd7a
Arsh Dawra <arshdawra7@gmail.com>
2025-12-28 01:55:28 +0530

    final

--- src/models/Transaction.js
+++ src/models/Transaction.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
-
 const transactionschema = new mongoose.Schema({
   amount: { type: Number, required: true },
-  type: { type: String, required: true },
-  category: {String},
-  description: {String},
+  type: { type: String, required: true, enum: ["credit", "debit"] },
+
+  category: { String },
+  description: { String },
   date: { type: Date, default: Date.now },
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
 });
